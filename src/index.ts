2f5dd51a7030453867c6da1e5167e98d3ac1ab92
Lavric Razvan <razvan.lavric@gmail.com>
2021-06-26 12:19:38 +0300

    chore: fixed scripts to work on windows

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 
-export * from './utils';
+export * from './Utils';
 export * from './Types';
 
 import * as fs from 'fs';
-import { getSchemaObject, schemaObjectToTextString } from './utils';
+import { getSchemaObject, schemaObjectToTextString } from './Utils';
 import { obj } from './test/resources/Object';
 const schemaObject = getSchemaObject(obj);
 const schemaString = schemaObjectToTextString(schemaObject);
